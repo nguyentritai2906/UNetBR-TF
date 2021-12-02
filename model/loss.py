37f2312b62202419139a821341fe560e58d69f94
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2021-12-02 15:39:46 +0700

    x

--- model/loss.py
+++ model/loss.py
@@ -2,6 +2,5 @@ import tensorflow as tf
 
 
 def HeSho(y_preds, y_true):
-    losses = tf.reduce_mean((y_true - y_preds)**2)
-    loss = tf.reduce_mean(tf.stack(losses))
+    loss = tf.reduce_mean((y_true - y_preds)**2)
     return loss
